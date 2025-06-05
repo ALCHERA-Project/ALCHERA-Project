2a9504113e6522aaf128da1163796b63e2e0e796
joys2225 <joys2225@naver.com>
2025-06-05 20:23:29 +0900

    sub1 수정

--- global.js
+++ global.js
@@ -1,3 +1,14 @@
+/* header 햄버거 메뉴 */
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const menu = document.getElementById('slide-menu');
+
+  hamburgerMenu.addEventListener('click', function () {
+    menu.classList.toggle('active');
+    hamburgerMenu.classList.toggle('active');
+  });
+});
+
 // menu scroll
 document.addEventListener("DOMContentLoaded", function () {
   const menuItems = document.querySelectorAll(".main-menu > li");
