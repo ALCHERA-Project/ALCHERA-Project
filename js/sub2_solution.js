86e516c70061826ecf3a513499096fc7f3c93909
joys2225 <joys2225@naver.com>
2025-06-29 20:35:42 +0900

    photoDump 수정

--- js/sub2_solution.js
+++ js/sub2_solution.js
@@ -34,3 +34,27 @@ function updateSlider() {
       }
     });
   });
+
+  // 스크롤하면 투명도 1로 변경, 위로 떠오르는 이벤트
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const titleElements = gsap.utils.toArray('.con1-subTitle, .con1-title, .txt-box, .con2-subTitle, .con2-title, .con3-subTitle, .con3-title, .arrow, .con4-subTitle, .con4-title, .list');
+
+  titleElements.forEach((el) => {
+    gsap.set(el, { opacity: 0, y: 50 });
+
+    gsap.to(el, {
+      opacity: 1,
+      y: 0,
+      duration: 0.8,
+      scrollTrigger: {
+        trigger: el,
+        start: "top 90%",
+        toggleActions: "play none none reverse",
+        markers: false
+      }
+    });
+  });
+});
+
